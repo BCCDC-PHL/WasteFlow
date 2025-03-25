8f0f3b59b6c03ebb1ff9a042c000dd8dd0f5e4eb
j3551ca <39065261+j3551ca@users.noreply.github.com>
2025-03-25 15:26:30 -0700

    Remove extra quote

--- bin/mutation_watchlist.R
+++ bin/mutation_watchlist.R
@@ -33,7 +33,7 @@ if ( is.null(opt$concat_mutations) | is.null(opt$outdir) ){
 }
 
 message("Running script with the following options:")
-message(sprintf("--concat_mutations : %s\n--outdir : %s\n--DT_threads : %s"",
+message(sprintf("--concat_mutations : %s\n--outdir : %s\n--DT_threads : %s",
                 opt$concat_mutations, opt$outdir, opt$DT_threads))
 
 
